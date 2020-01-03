26ecb2c07488f486c6bf512c50f80dfdf580b32b
Kevin O'Brien <kpobrien@buffalo.edu>
2020-01-03 12:13:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+function findMinAndRemoveSorted(array){
+    let min = array[0]
+    let minIndex = 0
+    for(let i = 0; i < array.length; i++){
+      if(array[i] < min){
+        min = array[i]
+        minIndex = i
+      }
+    }
+    array.splice(minIndex, 1);
+    return min;
+}
 
+function merge(firstHalf, secondHalf){
+    let sorted = []
+    while(firstHalf.length != 0 && secondHalf.length != 0){
+      let currentMin = findMinAndRemoveSorted(firstHalf, secondHalf)
+      sorted.push(currentMin)
+    }
+    return sorted.concat(firstHalf).concat(secondHalf)
+}
+
+
+function mergeSort(array){
+    let midpoint = (array.length)/2
+    let firstHalf = array.slice(0,midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+    if (array.length < 2){
+        return array
+    }
+    else {
+    merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+}
