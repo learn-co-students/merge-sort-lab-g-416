e87dde7bdd80baced52c85e9a0e57cf3f61c992e
Doug Kintop <dkintop@hotmail.com>
2020-03-17 16:47:16 -0400

    test

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+function findMinAndRemoveSorted(array) {
+  let min = 100000000;
+  let minIndex;
+  for (i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
+}
