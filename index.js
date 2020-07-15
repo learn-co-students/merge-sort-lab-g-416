1f343628949b2ecb52e11c5d0d3491f601315f07
Haj B <phajib@gmail.com>
2020-07-15 14:48:17 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function findMinAndRemoveSorted(array) {
+    return array.shift()
+}
 
+function merge(firstArray, secondArray) {
+    let newArray = [];
+
+    while (firstArray.length != [0] && secondArray.length != [0]) {
+        if(firstArray[0] < secondArray[0]) {
+            newArray.push(findMinAndRemoveSorted(firstArray))
+        } else {
+            newArray.push(findMinAndRemoveSorted(secondArray))
+        }
+    }
+    return newArray.concat(firstArray).concat(secondArray)
+}
+
+function mergeSort(array){
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+    let newArray
+ 
+    if(array.length < 2){
+      return array
+    } else {
+      newArray = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+    return newArray
+}
