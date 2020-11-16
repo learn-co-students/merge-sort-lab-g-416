bc01f4abc031dff7a41e265ee01310d0bcb9ab53
Wendy Harris <wendyv510@hotmail.com>
2020-11-16 14:39:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function findMinAndRemoveSorted(firstHalf, secondHalf){
+    let minFirstHalf = firstHalf[0];
+    let minSecondHalf = secondHalf[0];
+    if (minFistHalf < minSecondHalf){
+        return firstHalf.shift()
+    } else {
+        return secondHalf.shift()
+    }
+}
 
+function mergeSort(firstHalf, secondHalf){
+    let sorted = []
+    while (firstHalf.lenght != 0 && secondHalf.length !=0){
+        let currentMin = findMinAndRemoveSorted(firstHalf,secondHalf)
+        sorted.push(currentMin)
+    }
+    return sorted.concat(firstHalf).concat(secondHalf)
+}
