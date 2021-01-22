28f1cab354daf5ecb28eca818cf477231ac1bfd0
NickSpangler <Spangleyman@mac.com>
2021-01-22 10:02:48 -0500

    solve findMinAndRemoveSorted

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function findMinAndRemoveSorted(array) {
+    return array.shift()
+}
 
+function merge(array1, array2) {
+    let merged = [];
+    while (array1.length != 0 && array2.length != 0) {
+        if (array1[0] < array2[0]) {
+            merged.push(array1.shift())
+        } else {
+            merged.push(array2.shift())
+        }
+    }
+    return merged.concat(array1).concat(array2);
+}
