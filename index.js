62d535468f9182c1d1687ae21feae4bc107f02b1
ViolaCello <ViolaCelloNYC@gmail.com>
2021-01-29 14:55:28 -0500

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,30 @@ function findMinAndRemoveSorted(array) {
     array.splice(index, 1)
     return min
 }
+
+function merge(arrOne, arrTwo) {
+    let sorted = []
+    while (arrOne.length !=0 && arrTwo.length !=0) {
+        if (arrOne[0] < arrTwo[0]) {
+            sorted.push(arrOne[0])
+            arrOne.splice(0, 1)
+        } else {
+            sorted.push(arrTwo[0])
+            arrTwo.splice(0,1)
+        }
+    }
+
+    return sorted.concat(arrOne, arrTwo)
+}
+
+function mergeSort(array) {
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0, midpoint)
+    let lastHalf = array.slice(midpoint, array.length)
+    console.log("FirstH=",firstHalf," secondH=", lastHalf)
+    if (array.length < 2 ) {
+        return array
+    } else {
+        return merge(mergeSort(firstHalf), mergeSort(lastHalf))
+    }
+}
