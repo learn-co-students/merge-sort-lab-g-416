be87dd972f9a1cccc02b7750d530bc920691064b
ViolaCello <ViolaCelloNYC@gmail.com>
2021-01-29 14:30:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+function findMinAndRemoveSorted(array) {
+    let min=array[0]
+    let index = 0
+    for (let i = 1; i<array.length; i++) {
+        if (array[i] < min) {
+            min = array[i]
+            index = i
+        }
+    }
+    array.splice(index, 1)
+    return min
+}
