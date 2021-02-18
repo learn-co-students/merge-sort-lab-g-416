24d8a65a3c327eb0342002d8d86a76024b8eafd9
Brycew30 <brycew30@gmail.com>
2021-02-18 09:13:42 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function findMinAndRemoveSorted(arr) {
+  return arr.shift();
+}
 
+function merge(first, second) {
+  let sorted = [];
+  while (first.length > 0 && second.length > 0) {
+    if (first[0] < second[0]) {
+      sorted.push(findMinAndRemoveSorted(first))
+    } else {
+      sorted.push(findMinAndRemoveSorted(second))
+    }
+  }
+  return sorted.concat(first).concat(second);
+}
+
+function mergeSort(arr) {
+  let halfway = arr.length/2;
+  let first = arr.slice(0, halfway);
+  let second = arr.slice(halfway, arr.length);
+  let sorted;
+  if (arr.length < 2) {
+    return arr
+  } else {
+    sorted = merge(mergeSort(first), mergeSort(second))
+  }
+  return sorted;
+}
