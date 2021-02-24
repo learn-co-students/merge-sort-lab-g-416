865a17cf3a8070f4f6f4232f3522ed451cb32bfd
LeahESc <leahemily1@gmail.com>
2021-02-24 11:17:35 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 
+function findMinAndRemoveSorted(array) { 
+    return array.shift()
+}
+
+function merge(arrayOne, arrayTwo) { 
+    let sorted = []
+    while ( arrayOne.length != 0 && arrayTwo.length != 0 ){ 
+        let currentMin
+        if (arrayOne[0] < arrayTwo[0]) { 
+            currentMin = arrayOne[0] 
+            findMinAndRemoveSorted(arrayOne)    
+        } else { 
+            currentMin = arrayTwo[0] 
+            findMinAndRemoveSorted(arrayTwo)   
+        }
+        sorted.push(currentMin)
+    }
+    return sorted.concat(arrayOne).concat(arrayTwo)
+}
+
+function mergeSort(array) { 
+    let midPoint = array.length/2 
+    let arrayOne = array.slice(0, midPoint)
+    let arrayTwo = array.slice(midPoint, array.length)
+    if (array.length < 2) { 
+        return array
+    } else 
+
+    return merge(mergeSort(arrayOne), mergeSort(arrayTwo))
+}
