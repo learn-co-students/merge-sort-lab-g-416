ed9907ee9f1aae898e775c0b27101449e96c2869
Olesya Miller <olesyam.miller@gmail.com>
2021-02-27 15:48:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function findMinAndRemoveSorted(array) {
+    let min = array[0]
+    for(let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i]
+        }
+    }
+    array.splice(array.indexOf(min), 1)
+    return min 
+}
 
+function merge(array1, array2) {
+    let newArray = []
+    while(array1.length != 0 && array2.length != 0) {
+       let array1Min = array1[0]
+       let array2Min = array2[0]
+       if (array1Min < array2Min) {
+           newArray.push(array1Min)
+           array1.splice(array1.indexOf(array1Min), 1)
+       } else {
+           newArray.push(array2Min)
+           array2.splice(array2.indexOf(array2Min), 1)
+       }
+    }
+    let merged = newArray.concat(array1).concat(array2)
+    return merged 
+   
+}
