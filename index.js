51847a8b6795f4ab2e2c7f7e96f131ddb3827307
Alexander M. Clamor <alexander.clamor@gmail.com>
2021-03-26 01:47:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function findMinAndRemoveSorted (arr) {
+    return arr.shift()
+}
 
+function merge (firstArr, secondArr) {
+    let sortedArray = []
+
+    while (firstArr.length !== 0 && secondArr.length !== 0) {
+        if (firstArr[0] < secondArr[0]) {
+            sortedArray.push(findMinAndRemoveSorted(firstArr))
+        } else {
+            sortedArray.push(findMinAndRemoveSorted(secondArr))
+        }
+    }
+    return sortedArray.concat(firstArr).concat(secondArr)
+}
+
+function mergeSort (arr) {
+
+    let mid = arr.length/2
+    let firstArr = arr.slice(0, mid)
+    let secondArr = arr.slice(mid, arr.length)
+
+    if (arr.length < 2) {
+        return arr
+    } else {
+        return merge(mergeSort(firstArr), mergeSort(secondArr))
+    }
+}
