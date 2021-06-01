946599e133f83ff2f415b92fe6ca700d24fb6ef0
Jason J Cruz <jaycru06@yahoo.com>
2021-06-01 22:32:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 
+/* ex: 
+   let firstSubarray = [3, 4, 6, 7]
+   let secondSubArray = [1, 2, 5, 8]
+*/
+
+// find and remove smallest value in a sorted array
+
+function findMinAndRemoveSorted(array) {
+  let min = array[0];
+  let minIndex = 0;
+  
+  for (const value of array) {
+    if (value < min) {
+      min = value;
+      minIndex = array.indexOf(value);
+    }
+  }
+  array.splice(minIndex, 1);
+  return min;
+}
+
+// implement merge to combine two sub arrays
+function merge(firstSubArray, secondSubArray) {
+  let sorted = [];
+  let min;
+  while (firstSubArray.length !== 0 && secondSubArray.length !== 0) {
+     
+     min = findMinAndRemoveSorted([firstSubArray, secondSubArray]);
+     sorted.push(min);
+   
+  }
+  // concat result of values left in arrays
+  return sorted.concat(firstSubArray).concat(secondSubArray);
+}
+
+
+
+
+
+
+
+
+
+
+
+
