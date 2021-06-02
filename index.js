9cf368bf3417bf0554672b1c3ec041920f70cc07
Jason J Cruz <jaycru06@yahoo.com>
2021-06-02 02:07:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 
+function findMinAndRemoveSorted(array){
+  return array.shift()
+}
+
+function merge(firstSortedSubArray, secondSortedSubArray) {
+  let sorted = [];
+  while (firstSortedSubArray.length !== 0 &&
+         secondSortedSubArray.length !== 0) {
+         if (firstSortedSubArray[0] < secondSortedSubArray[0]) {
+           sorted.push(findMinAndRemoveSorted(firstSortedSubArray));
+         }  else {
+           sorted.push(findMinAndRemoveSorted(secondSortedSubArray));
+         }
+  }
+  return sorted.concat(firstSortedSubArray).concat(secondSortedSubArray);
+}
+
+
+function mergeSort(array) {
+  let middle = array.length / 2;
+  let firstHalf = array.slice(0, middle);
+  let secondHalf = array.slice(middle, array.length);
+  
+  if (array.length < 2) {
+    return array;
+  } else {
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+  }
+}
+
+
+
+
+
+
+
